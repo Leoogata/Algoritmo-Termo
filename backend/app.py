31c36fa79aa1530ed2da0af7b8ac4bff4a9036b7
Leoogata <leonardo.pedrosa@sou.inteli.edu.br>
2025-11-12 14:23:56 -0300

    feat: adiciona coneccao com o banco de dados na analise de dados

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify, request, abort
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
-
+from services.termo_service import add_termo, get_termos
+from services.dueto_service import add_dueto, get_duetos
+from services.quarteto_service import add_quarteto, get_quartetos
 # Inicializar a aplicação Flask
 app = Flask(__name__)
 
